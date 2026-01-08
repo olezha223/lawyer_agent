cbad774f910be46295211f9a8f055c4438b7f633
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2026-01-09 02:14:05 +0300

    добавил конфиг

---
+++ app/src/shared/config.py
@@ -0,0 +1,22 @@
+import os
+
+from dotenv import load_dotenv
+from dataclasses import dataclass
+
+
+load_dotenv()
+
+
+@dataclass
+class Config:
+    OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
+    ALLOW_ORIGINS: str = os.getenv("ALLOW_ORIGINS", "*")
+    ALLOW_CREDENTIALS: str = os.getenv("ALLOW_CREDENTIALS", "*")
+    ALLOW_METHODS: str = os.getenv("ALLOW_METHODS", "*")
+    ALLOW_HEADERS: str = os.getenv("ALLOW_HEADERS", "*")
+
+    APP_CORS_ORIGINS_LIST = os.getenv('APP_CORS_ORIGINS_LIST', default='').split(',')
+    APP_NGINX_PREFIX: str = os.getenv('APP_NGINX_PREFIX', default='/')
+
+
+config = Config()
