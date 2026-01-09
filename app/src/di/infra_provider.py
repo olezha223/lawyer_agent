f53d3eec57cc85e22b0d111a1dac061b82096730
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2026-01-10 02:23:27 +0300

    обновил di

--- app/src/di/infra_provider.py
+++ app/src/di/infra_provider.py
@@ -1,7 +1,9 @@
 from dishka import Provider, Scope, provide
 
+from src.domain.services.iembedder import IEmbedder
 from src.domain.services.ipdf_adapter import IPdfAdapter
 from src.domain.services.ivdb_adapter import IVectorDB
+from src.infra.embeddings.bge import BGEEmbedder
 from src.infra.pdf.pypdf2_adapter import PyPdf2Adapter
 from src.infra.vdb.qdrant_adapter import QdrantAdapter
 
@@ -11,3 +13,4 @@ class InfraProvider(Provider):
 
     pdf_adapter = provide(PyPdf2Adapter, provides=IPdfAdapter)
     vdb_adapter = provide(QdrantAdapter, provides=IVectorDB)
+    embedder_provider = provide(BGEEmbedder, provides=IEmbedder)
--- app/src/di/tools_provider.py
+++ app/src/di/tools_provider.py
@@ -1,9 +1,11 @@
 from dishka import Provider, Scope, provide
 
 from src.application.tools import AnalyzeRiskTool
+from src.application.tools.relevant_search import RagTool
 
 
 class ToolsProvider(Provider):
     scope = Scope.REQUEST
 
     analyze_risk_tool = provide(AnalyzeRiskTool, provides=AnalyzeRiskTool)
+    rag_tool = provide(RagTool, provides=RagTool)
