9c4bca3c88323a262dfd7c369b8d42d38b6e5837
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2026-01-10 23:06:14 +0300

    мвп с двумя тулзами

--- app/src/application/agent/llm_agent_orchestrator.py
+++ app/src/application/agent/llm_agent_orchestrator.py
@@ -3,9 +3,14 @@ from typing import Optional
 from langchain_core.messages import BaseMessage
 from langchain_gigachat import GigaChat
 from langgraph.prebuilt import create_react_agent
+from qdrant_client import QdrantClient
 
 from src.application.tools import AnalyzeRiskTool
+from src.application.tools.relevant_search import RagTool
 from src.domain.services.ipdf_adapter import IPdfAdapter
+from src.infra.embeddings.bge import BGEEmbedder
+from src.infra.pdf.pypdf2_adapter import PyPdf2Adapter
+from src.infra.vdb.qdrant_adapter import QdrantAdapter
 from src.shared.config import config
 
 
@@ -16,9 +21,11 @@ class LLMAgentOrchestrator:
             pdf_adapter: IPdfAdapter,
             # tools
             analyze_risk_tool: AnalyzeRiskTool,
+            rag_tool: RagTool
     ):
         self.pdf_adapter = pdf_adapter
         self.analyze_risk_tool = analyze_risk_tool
+        self.rag_tool = rag_tool
 
     def _prepare_messages(self, user_prompt: str, document_text: str = "") -> list[BaseMessage | dict[str, str]]:
         system_prompt = (
@@ -38,12 +45,12 @@ class LLMAgentOrchestrator:
 
     def get_agent(self):
         model = GigaChat(
-            model="GigaChat-2-Max",
+            model="GigaChat-2-Pro",
             verify_ssl_certs=False,
             credentials=config.GIGACHAT_API_KEY,
             scope='GIGACHAT_API_PERS'
         )
-        tools = [self.analyze_risk_tool]
+        tools = [self.analyze_risk_tool, self.rag_tool]
         agent = create_react_agent(model=model, tools=tools)
         return agent
 
@@ -63,20 +70,22 @@ class LLMAgentOrchestrator:
         if pdf_bytes:
             document_text = self.pdf_adapter.parse_bytes(pdf_bytes)
         agent = self.get_agent()
-        response = agent.invoke(self._prepare_messages(text, document_text))
+        response = agent.invoke(self._prepare_messages(text, document_text), {"recursion_limit": 100})
         return response
 
 
 if __name__ == "__main__":
+    qdrant = QdrantAdapter(client=QdrantClient(host='localhost', port=6333))
     orch = LLMAgentOrchestrator(
-        pdf_adapter=IPdfAdapter(),
-        analyze_risk_tool=AnalyzeRiskTool()
+        pdf_adapter=PyPdf2Adapter(),
+        analyze_risk_tool=AnalyzeRiskTool(),
+        rag_tool=RagTool(vdb_adapter=qdrant, embedder=BGEEmbedder())
     )
 
     with open('../../../examples/data/Договор 10.00.02.26.03 Швецов Олег Андреевич.pdf', 'rb') as f:
         file_bytes = f.read()
 
     print(orch.execute(
-        text='какие основные пункты в договоре?',
+        text='Что ты можешь сказать о соответствии этого договора гражданскому кодексу?',
         pdf_bytes=file_bytes
     )['messages'][-1].content)
